9a503eadfe7df78daf137d2ad5d18a6768490f2b
Pavan Kumar Singh <pvnkmrsingh003@gmail.com>
2018-09-14 21:15:59 +0530

    Added ComputerScience.cs from remote repository

---
+++ ComputerScience.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Demo
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Learning Computer Science is FUN!!!");
+      COnsole.ReadKey();
+    }
+  }
+}
